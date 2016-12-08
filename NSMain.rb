238a10f16e60ccfbdbc54e3083b2b9fba4496b7b
Natanael Rivera <njrivera@mail.usf.edu>
2016-12-08 18:24:17 -0500

    done

--- NSMain.rb
+++ NSMain.rb
@@ -7,29 +7,53 @@ get '/' do
 end
 
 get '/name' do
-  if params['text'].match(/^[a-zA-Z]+$/)
-    erb :name_not_found
-  end
-  name = params['text'].srip
-  if Name.first(:name => name)
-    content_type :json
-    arr = [
-        Name.first(:name => name).name,
-        Name.first(:name => name).decade1900.to_s,
-        Name.first(:name => name).decade1910.to_s,
-        Name.first(:name => name).decade1920.to_s,
-        Name.first(:name => name).decade1930.to_s,
-        Name.first(:name => name).decade1940.to_s,
-        Name.first(:name => name).decade1950.to_s,
-        Name.first(:name => name).decade1960.to_s,
-        Name.first(:name => name).decade1970.to_s,
-        Name.first(:name => name).decade1980.to_s,
-        Name.first(:name => name).decade1990.to_s,
-        Name.first(:name => name).decade2000.to_s,
-    ]
 
-    { :result => arr }.to_json
+  name = params['text'].strip
+  if name.match(/^[a-zA-Z]+$/)
+    name = name.capitalize
+    if Name.first(:name => name)
+      content_type :json
+      if
+        Name.first(:name => name).decade1900 < 1 || Name.first(:name => name).decade1900 > 1001 ||
+        Name.first(:name => name).decade1910 < 1 || Name.first(:name => name).decade1910 > 1001 ||
+        Name.first(:name => name).decade1920 < 1 || Name.first(:name => name).decade1920 > 1001 ||
+        Name.first(:name => name).decade1930 < 1 || Name.first(:name => name).decade1930 > 1001 ||
+        Name.first(:name => name).decade1940 < 1 || Name.first(:name => name).decade1940 > 1001 ||
+        Name.first(:name => name).decade1950 < 1 || Name.first(:name => name).decade1950 > 1001 ||
+        Name.first(:name => name).decade1960 < 1 || Name.first(:name => name).decade1960 > 1001 ||
+        Name.first(:name => name).decade1970 < 1 || Name.first(:name => name).decade1970 > 1001 ||
+        Name.first(:name => name).decade1980 < 1 || Name.first(:name => name).decade1980 > 1001 ||
+        Name.first(:name => name).decade1990 < 1 || Name.first(:name => name).decade1990 > 1001 ||
+        Name.first(:name => name).decade2000 < 1 || Name.first(:name => name).decade2000 > 1001
+        content_type :json
+        { :result => 'Invalid Data From Database' }.to_json
+      else
+        arr = [
+            Name.first(:name => name).name,
+            Name.first(:name => name).decade1900.to_s,
+            Name.first(:name => name).decade1910.to_s,
+            Name.first(:name => name).decade1920.to_s,
+            Name.first(:name => name).decade1930.to_s,
+            Name.first(:name => name).decade1940.to_s,
+            Name.first(:name => name).decade1950.to_s,
+            Name.first(:name => name).decade1960.to_s,
+            Name.first(:name => name).decade1970.to_s,
+            Name.first(:name => name).decade1980.to_s,
+            Name.first(:name => name).decade1990.to_s,
+            Name.first(:name => name).decade2000.to_s,
+        ]
+        { :result => arr }.to_json
+      end
+    else
+      content_type :json
+      { :result => 'Name Not Found' }.to_json
+    end
   else
-    erb :name_not_found
+    content_type :json
+    { :result => 'Invalid Characters' }.to_json
   end
+end
+
+not_found do
+  erb :page_not_found
 end
